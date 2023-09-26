7f69768f00d756218e8a077614bba274fa2e3c0f
ajitroshan <ajit4roshan@gmail.com>
2023-09-26 18:52:21 +0530

    pass double parametr from dataProvider

--- src/test/java/testNG_A/P_one.java
+++ src/test/java/testNG_A/P_one.java
@@ -6,15 +6,15 @@ import org.testng.annotations.Test;
 public class P_one  {
 
 	@Test(dataProvider= "dataSet" )
-	public void baseTest(Object rData) {
-		System.out.println("baseTest : "+rData);
+	public void baseTest(Object rData1,Object rData2) {
+		System.out.println("baseTest : "+rData1+" and "+rData2);
 	}
 	
 	@DataProvider(name= "dataSet")
 	public Object[][] dataCollection() {
 		
 		Object[][] coll= {
-				{"ajit"},{"roshan"},{"soy"},{"ranchi"},{"dhanbad"}
+				{"ajit","1"},{"roshan","2"},{"soy","3"},{"ranchi","4"},{"dhanbad","5"}
 				};
 				
 				
